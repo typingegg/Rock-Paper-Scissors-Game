5081876af0afeac19fa3879e3bf8636ddcf4dae8
typingegg <109323593+typingegg@users.noreply.github.com>
2025-09-12 13:58:32 -1000

    i think i finished rock paper scissors. taking a break

--- script.js
+++ script.js
@@ -5,9 +5,6 @@
 // Computer can only get rock, paper, scissors.
 // Use Math.random(). It returns values between 0 and 1. So make rock between 0 and 0.33, paper between .34 and .66, scissors between 0.67 and 1. ✅
 
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice() {
     let randomWholeNumber = Math.floor(Math.random()*100);
 
@@ -30,13 +27,17 @@ function getHumanChoice() {
     return userChoiceLowerCase;
 }
 
-// do 5 rounds
+
 // get choices from getComputerChoice and getHumanChoice. ✅
 // set rules of what wins between rock, paper and scissors
 // if rock and paper, paper wins. if paper and scissors, paper wins. if rock and scissors, rock wins.
 // keep track of scores
+
+
 function playRound(humanChoice, computerChoice) {
 
+    let computerScore = 0;
+    let humanScore = 0;
     let human = humanChoice.toLowerCase();
     let computer = computerChoice.toLowerCase();
     
@@ -46,7 +47,7 @@ function playRound(humanChoice, computerChoice) {
         console.log("Computer shows a " + computer + ".");    
         console.log(human + " beats " + computer);
         humanScore++;
-        console.log("Human scores " + humanScore + "point/s!");
+        console.log("Human scores " + humanScore + " point!");
     }
 
     else if (human=="paper" && computer=="rock") {
@@ -54,17 +55,15 @@ function playRound(humanChoice, computerChoice) {
         console.log("Computer shows a " + computer + ".");  
         console.log(human + " beats " + computer);
         humanScore++;
-        console.log("Human scores " + humanScore + "point/s!");
-
+        console.log("Human scores " + humanScore + " point!");
     }
     
     else if (human=="scissors" && computer=="paper") {
         console.log("Human shows a " + human + ".");
         console.log("Computer shows a " + computer + ".");  
         console.log(human + " beats " + computer);
+        console.log("Human scores " + humanScore + " point!");
         humanScore++;
-        console.log("Human scores " + humanScore + "point/s!");
-
     }
 
     else if (human==computer) {
@@ -73,26 +72,49 @@ function playRound(humanChoice, computerChoice) {
         console.log(human + " and " + computer + " are the same so no point!");
     }
 
+    // computer wins
     else {
         console.log("Human shows a " + human + ".");
         console.log("Computer shows a " + computer + ".");  
         console.log(computer + " beats " + human);
-        computerScore++;
-        console.log("Computer scores " + computerScore + "point/s!");
-
+        computerScore++
+        console.log("Computer scores " + computerScore + " point!");
     }
+    return [humanScore, computerScore];
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+function playGame(){
 
-playRound(humanSelection, computerSelection);
-// console.log('Hello'.toLowerCase());
+    let humanScore = 0;
+    let computerScore = 0;
+    for (i = 0; i<5; i++) {
 
-// function playGame(playRound){
-//     for let(i = 0; i>5; i++){
-//         return;
-//     }
-// }
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
 
-// playGame();
+        scores = playRound(humanSelection, computerSelection);
+        if (scores[0]!=0) {
+            humanScore++;
+        }
+        if (scores[1]!=0) {
+            computerScore++;
+        }
+    }
+
+    console.log(humanScore,computerScore);
+
+}
+
+playGame();
+
+function playSumthing(){
+    for (i = 0; i < 5; i++) {
+        prompt("What's");
+    }
+}
+// playSumthing(); 
+
+// ask how many rounds are we playing?
+// start the first round, ask for the human choice, print the results of the first round..
+// start the second round, ask for the human choice, print the results..
+// so on until 5 rounds
